3d111192f6b0d0f6e93ffd36fe75466b5ad447e4
root <root@DESKTOP-97JCISV.localdomain>
2022-10-28 06:24:49 +0300

    добавил компонент для обработки ошибок

---
+++ packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { Component, ReactNode, ErrorInfo } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+  
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <div>Что-то пошло не так...</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
