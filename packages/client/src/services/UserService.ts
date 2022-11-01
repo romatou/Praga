bd87552108ebc8872b75efa161b7d5492dcca307
EkaterinaBelonosova <katerina2594@mail.ru>
2022-11-02 00:15:07 +0700

    добавление api

---
+++ packages/client/src/services/UserService.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const http = 'https://ya-praktikum.tech/api/v2';
+
+export default axios.create({
+  baseURL: http,
+  headers: {'Content-Type': 'application/json'},
+  responseType: 'json'
+});
