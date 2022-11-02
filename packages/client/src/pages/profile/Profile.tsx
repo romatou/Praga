e967daef8b95dd62de8755056fdf477d2d3530c7
EkaterinaBelonosova <katerina2594@mail.ru>
2022-11-03 02:54:15 +0700

    добавлено сохранение формы

--- packages/client/src/pages/profile/Profile.tsx
+++ packages/client/src/pages/profile/Profile.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import * as RB from '@mui/material'
-import axios from 'axios'
 import { LoadingButton } from '@mui/lab';
 import { useForm, FormProvider, Controller } from 'react-hook-form';
-import API from '../../services/UserService';
+import API from '../../services/BaseApi';
+import { editAvatarApi, editProfile, getUser } from '../../services/UserService';
+
 
 const inputForm = [
-  {name: 'firstName', label: 'Имя'},
-  {name: 'secondName', label: 'Фамилия'},
-  {name: 'displayName', label: 'Имя в чате'},
+  {name: 'first_name', label: 'Имя'},
+  {name: 'second_name', label: 'Фамилия'},
+  {name: 'display_name', label: 'Имя в чате'},
   {name: 'email', label: 'Почта'},
   {name: 'phone', label: 'Телефон'},
   {name: 'login', label: 'Логин'}
@@ -16,30 +17,79 @@ const inputForm = [
 
 const Profile = () => {
   const [open, setOpen] = React.useState(false);
-  const [appState, setAppState] = useState();
-
+  const [appState, setAppState] = useState(
+    {
+      loading: false,
+      user: {
+        avatar: '',
+        display_name: '',
+        email: '',
+        first_name: '',
+        id: '',
+        login: '',
+        phone: '',
+        second_name: ''
+      },
+    }
+  )
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
-    console.log(appState)
   };
   
-  
-  useEffect(() => {
-    API.get('/auth/user').then((resp) => {
-      console.log(resp);
+  const onSubmitAvatar = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
+    setAppState({loading: true, user: appState.user})
+    const payload = evt.target.files
+    const image = payload && payload[0]
+    if (image !== null) {
+      const formData = new FormData();
+      console.log(image)
+      formData.append('avatar', image);
+      editAvatarApi(formData)
+    }
+  }, [])
+  const onSubmitFormData = useCallback(async (value: any) => {
+    await editProfile(value)
+      .then((resp) => {
+        getUserData()
+      });
+  }, [])
+
+  const getUserData = () => {
+    getUser()
+    .then((resp) => {
       const getUser = resp.data;
-      setAppState(getUser);
+      setAppState({
+        loading: false,
+        user: getUser
+      });
     });
+  }
+  useEffect(() => {
+    setAppState({loading: true, user: appState.user})
+    getUserData()
   }, []);
 
+  useEffect(() => {
+    if (appState.user) {
+        reset({
+            display_name: appState.user.display_name,
+            email: appState.user.email,
+            first_name: appState.user.first_name,
+            login: appState.user.login,
+            phone: appState.user.phone,
+            second_name: appState.user.second_name
+        })
+    }
+  }, [appState.user]); 
+
   const methods = useForm();
-  const { register, handleSubmit } = methods;
+  const { register, handleSubmit, reset  } = methods;
   return (
-      <RB.Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <RB.Container sx={{ display: 'flex', flexDirection: 'column' }}>
         <RB.IconButton
           color='primary'
           aria-label='upload picture'
@@ -50,16 +100,17 @@ const Profile = () => {
             marginBottom: 4
           }}
         >
-          <input hidden accept='image/*' type='file' /*onChange={""}*/ />
+          <input id='avatar' name='avatar' hidden accept='image/*' type='file' onChange={onSubmitAvatar} />
 
           <RB.Avatar
             sx={{ width: 120, height: 120 }}
-           // src={}
+            alt="avatar"
+            src={"https://ya-praktikum.tech/api/v2/resources" + (appState.user?.avatar ?? '')}
           />
         </RB.IconButton>
 
         <FormProvider {...methods}>
-          <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <form onSubmit={handleSubmit(onSubmitFormData)}>
             <RB.Grid
               container
               direction="column"
@@ -67,20 +118,98 @@ const Profile = () => {
               alignItems="center"
               spacing={2}
             >
-              {inputForm.map((item, i) => {
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('first_name', { value: appState.user?.first_name })}
+                  label={(appState.user?.first_name !== null) ? '':'Имя'}
+                  value={appState.user?.first_name}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, first_name: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('second_name')}
+                  name='second_name'
+                  label={(appState.user?.second_name !== null) ? '':'Фамилия'}
+                  value={appState.user?.second_name}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, second_name: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('display_name')}
+                  name='display_name'
+                  label={(appState.user?.display_name !== null) ? '':'Имя в чате'}
+                  value={(appState.user?.display_name === null) ? '': appState.user?.display_name}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, display_name: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('email')}
+                  name='email'
+                  label={(appState.user?.email !== null) ? '':'Email'}
+                  value={appState.user?.email}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, email: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('login')}
+                  name='login'
+                  label={(appState.user?.login !== null) ? '':'Логин'}
+                  value={appState.user?.login}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, login: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              <RB.Grid item xs={12}>
+                <RB.TextField
+                  type='text'
+                  {...register('phone')}
+                  name='phone'
+                  label={(appState.user?.phone !== null) ? '':'Телефон'}
+                  value={appState.user?.phone}
+                  onChange={e => setAppState((prevState) => (
+                    {user: {...prevState.user, phone: e.target.value}, loading: false}
+                  ))}
+                  size="small"
+                />
+              </RB.Grid>
+              {/*inputForm.map((inp: any, i) => {
                 return (
                   <RB.Grid item xs={12} key={i}>
                     <RB.TextField
                       /*{...register(item.name, {
                         pattern: /[A-Za-z]{3}/
                       })}*/ //для валидации если нужно будет
-                      type='text'
-                      name={item.name}
-                      label={item.label}
+                      /*type='text'
+                      name={inp.name}
+                      label={inp.label}
+                      value={appState.user[inp.name]}
                     />
                   </RB.Grid>
                 )
-              })}
+              })*/}
               <RB.Grid item xs={12}>
                 <LoadingButton
                   size='small'
@@ -138,7 +267,7 @@ const Profile = () => {
           </form>
 
         </FormProvider>
-      </RB.Box>
+      </RB.Container>
   )
 }
 
---
+++ packages/client/src/services/BaseApi.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const http = 'https://ya-praktikum.tech/api/v2';
+
+export default axios.create({
+  baseURL: http,
+  headers: {'Content-Type': 'application/json'},
+  responseType: 'json',
+  withCredentials: true
+});
