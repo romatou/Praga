06cb953928824031c5779928f4be1503d63a199a
EkaterinaBelonosova <katerina2594@mail.ru>
2022-11-04 08:22:10 +0700

    добавлено изменение пароля

---
+++ packages/client/src/components/ModalPassword/index.tsx
@@ -0,0 +1,73 @@
+import React, { useCallback, useEffect } from 'react';
+import * as RB from '@mui/material'
+import { useForm, FormProvider} from 'react-hook-form';
+import { passwordType } from '../../services/UserService';
+
+
+const ModalPassword = (props: any) => {
+  const methods = useForm<passwordType>({
+    defaultValues: {
+      oldPassword: '',
+      newPassword: '',
+    },
+    mode: 'onBlur'
+  });
+  const { register, handleSubmit, reset } = methods;
+
+
+  useEffect(() => {
+      methods.reset({
+        oldPassword: '',
+        newPassword: ''
+      })
+    }, [])
+
+  return (
+      <RB.Container sx={{ display: 'flex', flexDirection: 'column' }}>
+        <RB.Dialog open={props.isopen} onClose={props.handleClose}>
+            <RB.DialogTitle>Изменить пароль</RB.DialogTitle>
+            <FormProvider {...methods}>
+                <form onSubmit={handleSubmit(props.onSubmitPassword)}>
+                    <RB.DialogContent>
+                        <RB.Grid
+                            container
+                            direction='column'
+                            justifyContent='center'
+                            alignItems='center'
+                            spacing={2}
+                        >
+                        <RB.Grid item xs={12}>
+                            <RB.TextField
+                                type='text'
+                                {...register('oldPassword')}
+                                label='Текущий пароль'
+                                size='small'
+                            />
+                        </RB.Grid>
+                        <RB.Grid item xs={12}>
+                            <RB.TextField
+                                type='text'
+                                {...register('newPassword')}
+                                label='Новый пароль'
+                                size='small'
+                            />
+                        </RB.Grid>
+                    </RB.Grid>
+                    </RB.DialogContent>
+                    <RB.DialogActions>
+                        <RB.Button 
+                            size='small'
+                            type='submit'
+                            onClick={props.handleClose}
+                        >
+                            Сохранить
+                        </RB.Button>
+                    </RB.DialogActions>
+                </form>
+            </FormProvider>
+        </RB.Dialog>
+    </RB.Container>
+  )
+}
+
+export default ModalPassword
