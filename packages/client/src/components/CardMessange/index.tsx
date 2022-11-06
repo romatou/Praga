bd6f629f486411770d987c68836d517628b856d3
EkaterinaBelonosova <katerina2594@mail.ru>
2022-11-07 01:17:58 +0700

    Верстка форума-макет

---
+++ packages/client/src/components/CardMessange/index.tsx
@@ -0,0 +1,29 @@
+import * as RB from '@mui/material'
+
+interface Props {
+    name: string,
+    text: string, 
+    data: string
+  }
+
+const CardMessange = (props: Props) => {
+   return (
+    <RB.Card sx={{ maxWidth: 478 }}>
+        <RB.CardContent>
+            <RB.Typography gutterBottom variant="subtitle2" component="div">
+                {props.name}
+            </RB.Typography>
+            <RB.Typography variant="body2" color="text.secondary">
+                {props.text}
+            </RB.Typography>
+        </RB.CardContent>
+        <RB.CardActions>
+            <RB.Typography variant="caption" color="inherit" sx={{marginLeft: 'auto'}}>
+                {props.data}
+            </RB.Typography>
+        </RB.CardActions>
+  </RB.Card>
+  )
+}
+
+export default CardMessange
--- packages/client/src/pages/forum/Forum.tsx
+++ packages/client/src/pages/forum/Forum.tsx
@@ -1,8 +1,63 @@
+import * as RB from '@mui/material'
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
-export default function Forum() {
-  return (
-    <>
-      <h1>Страница форума</h1>
-    </>
-  )
-}
+const forumData = [
+  {name: 'Возможности игры', count: 10, id: 5},
+  {name: 'Баги', count: 4, id: 1},
+  {name: 'Идеи', count: 8, id: 2},
+  {name: 'Новости', count: 20, id: 3},
+  {name: 'Жалобы', count: 10, id: 4},
+]
+const Item = styled(RB.Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  width: '478px'
+}));
+const Forum = () => (
+  <RB.Container maxWidth={false} sx={{ position: 'relative', height: '100vh', width: '100vw', background: '#D5D5D5'}}>
+    <RB.Container sx={{ display: 'flex', flexDirection: 'column' }}>
+      <RB.Grid
+        container
+        direction='column'
+        justifyContent='center'
+        alignItems='center'
+        spacing={2}
+        marginTop={4}
+      >
+        <RB.Grid item xs={12} sx={{width: '478px'}} spacing={12}>
+          <RB.Grid container >
+            <RB.Grid item>
+              Тема
+            </RB.Grid>
+            <RB.Grid item sx={{marginLeft: 'auto'}}>
+              Количество  комментариев
+            </RB.Grid>
+          </RB.Grid>
+        </RB.Grid>
+        {forumData.map((it, i) => {
+          return (
+          <RB.Grid item xs={12} key={i}>
+            <Item>
+              <Link to={"/forum/" + it.id} style={{color: 'inherit', textDecoration: 'none', }}>
+                  <RB.Grid container >
+                    <RB.Grid item>
+                      {it.name}
+                    </RB.Grid>
+                    <RB.Grid item sx={{marginLeft: 'auto'}}>
+                      {it.count}
+                    </RB.Grid>
+                  </RB.Grid>
+              </Link>
+            </Item>
+          </RB.Grid>
+        )})}
+      </RB.Grid>
+    </RB.Container>
+  </RB.Container>
+)
+
+export default Forum;
