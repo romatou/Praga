5397f5a10ff53aeeed9fb3782bb78f5d576d4957
Roma Tou <romatou@me.com>
2022-11-07 14:11:28 +0500

    Добавлены ссылки на страницы приложения на главную

--- packages/client/src/pages/intro/Intro.tsx
+++ packages/client/src/pages/intro/Intro.tsx
@@ -1,8 +1,42 @@
+import { Link } from 'react-router-dom'
 
 export default function Intro() {
   return (
-    <>
+    <div>
       <h1>Мини-лэндинг - Главная страница</h1>
-    </>
+      <p>Это временная страница-заглушка. Здесь позже будет мини-лэндинг</p>
+      <ul>
+        <li>
+          <Link to="/auth">Авторизация</Link>
+        </li>
+        <li>
+          <Link to="/register">Регистрация</Link>
+        </li>
+        <li>
+          <Link to="/profile">Профиль</Link>
+        </li>
+        <li>
+          <Link to="/ranking">Лидерборд</Link>
+        </li>
+        <li>
+          <Link to="/game/start">Начало игры</Link>
+        </li>
+        <li>
+          <Link to="/game/play">Игра</Link>
+        </li>
+        <li>
+          <Link to="/game/result">Завершение игры</Link>
+        </li>
+        <li>
+          <Link to="/forum">Форум</Link>
+        </li>
+        <li>
+          <Link to="/404">Страница не найдена</Link>
+        </li>
+        <li>
+          <Link to="/500">Ошибка на сервере</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
