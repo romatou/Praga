bd0d20c0b11820e3c7c7f15211c88d2e9f8f0a2e
Roma Tou <49362229+romatou@users.noreply.github.com>
2023-01-11 11:00:30 +0500

    Раскомментировал Service Worker

--- packages/client/src/entry-client.tsx
+++ packages/client/src/entry-client.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { RootState, setupStore } from './store'
 import ErrorBoundary from './components/ErrorBoundary'
+import { RootState, setupStore } from './store'
 
 const store = setupStore(window.__PRELOADED_STATE__)
 
@@ -14,21 +14,21 @@ declare global {
   }
 }
 
-// window.addEventListener('load', () => {
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//       .register('/sw.js')
-//       .then(registration => {
-//         console.log(
-//           'ServiceWorker registration successful with scope: ',
-//           registration.scope
-//         )
-//       })
-//       .catch((error: string) => {
-//         console.log('ServiceWorker registration failed: ', error)
-//       })
-//   }
-// })
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        )
+      })
+      .catch((error: string) => {
+        console.log('ServiceWorker registration failed: ', error)
+      })
+  }
+})
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
