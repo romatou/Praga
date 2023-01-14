aa7d4f74e2de723c98264d8c4678c26f0dc01cd8
Roma Tou <49362229+romatou@users.noreply.github.com>
2023-01-14 09:41:57 +0500

    Исправление ошибок работы приложения, добавлены изменения в Dockerfile

--- packages/client/src/hooks/useAuth.ts
+++ packages/client/src/hooks/useAuth.ts
@@ -1,21 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '../hooks/useUser'
 import { fetchUser } from '../store/actions/UserActionCreators'
 import { useAppDispatch } from '../store/index'
-import { useUser } from '../hooks/useUser'
-import { useNavigate } from 'react-router-dom'
 
 export const useAuth = () => {
   const user = useUser()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
-  
+
   return () => {
-    if (!user.id) {
-      dispatch(fetchUser()).then(res => {
-        if (res.type === 'auth/fetch/rejected') {
+    user.userData.id
+      ? navigate('/game/start')
+      : dispatch(fetchUser()).then(res => {
+        if (res.type === '/user/fetchUser/rejected') {
           return navigate('/auth')
         }
       })
-    }
-    return
   }
 }
--- packages/client/src/pages/intro/Intro.tsx
+++ packages/client/src/pages/intro/Intro.tsx
@@ -1,19 +1,16 @@
 import { Box, Button, Container, Typography } from '@mui/material'
 import { useEffect } from 'react'
-import { useOAuth } from '../../hooks/useOAuth'
+import { useAuth } from '../../hooks/useAuth'
 import { fetchUser } from '../../store/actions/UserActionCreators'
 import { useAppDispatch } from '../../store/index'
 
-import { useAuth } from '../../hooks/useAuth'
 import styles from './styles'
-import { Link } from 'react-router-dom'
 
 export default function Intro() {
-  const getOAuth = useOAuth()
+  const getAuth = useAuth()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    if (getOAuth) getOAuth()
     dispatch(fetchUser())
   }, [])
 
@@ -21,29 +18,26 @@ export default function Intro() {
     <Box sx={{ ...styles.page }}>
       <Container sx={{ height: '100vh' }}>
         <Box sx={{ display: 'flex', flexFlow: 'column', height: '100vh' }}>
-            <Box
-              sx={{
-                ...styles.containerCentered,
-                flexGrow: '2',
-                alignItems: 'flex-start',
-              }}>
-              <Link to="/game/start" style={styles.link}>
-                <Button
-                  sx={{
-                    background: '#000',
-                    marginBottom: '1rem',
-                    ':hover': {
-                      backgroundColor: '#000',
-                      color: '#fff',
-                    },
-                  }}>
-                  Начать игру
-                </Button>
-              </Link>
-              <Typography>Для игры требуется регистрация</Typography>
-            </Box>
+          <Box
+            sx={{
+              ...styles.containerCentered,
+              flexGrow: '2',
+              alignItems: 'flex-start',
+            }}>
+            <Button
+              onClick={getAuth}
+              sx={{ marginBottom: '1rem' }}
+              variant="contained"
+              size="large"
+              color="secondary">
+              Начать игру
+            </Button>
+            <Typography sx={{ color: 'black' }}>
+              Для игры требуется регистрация
+            </Typography>
           </Box>
-        </Container>
+        </Box>
+      </Container>
       <Box
         sx={{
           height: '100vh',
@@ -127,4 +121,3 @@ export default function Intro() {
     </Box>
   )
 }
-
